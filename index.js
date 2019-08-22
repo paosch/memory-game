b8b6001871f34c00e2cab8f677bb8aa60f7b4895
Paola Schiaffino <paola_schiaffino@yahoo.co.uk>
2019-08-22 15:42:07 +0100

    Created 16 flipcards

--- index.js
+++ index.js
@@ -1,51 +1,27 @@
-// console.log("Chris eats too much")
-// <div class="flip-card">
-//   <div class="flip-card-inner">
-//     <div class="flip-card-front">
-//       <img src="https://www.w3schools.com/howto/img_avatar.png" alt="Avatar" class="card-img">
-//     </div>
-//     <div class="flip-card-back">
-//       <h1>John Doe</h1>
-//       <p>Architect & Engineer</p>
-//       <p>We love that guy</p>
-//     </div>
-//   </div>
-// </div>
+const colors = ["dodgerblue", "red", "green", "pink", "orange", "yellow", "black", "navy"]
+colors.forEach(function(color){
+  for (let i=0; i<2; i++){
 
+    const flipCard = document.createElement("div");
+    flipCard.classList.add("flip-card");
 
+    const flipCardInner = document.createElement("div");
+    flipCardInner.classList.add("flip-card-inner");
 
-const flipCard = document.createElement("div");
-flipCard.classList.add("flip-card");
+    flipCard.appendChild(flipCardInner);
+    document.querySelector(".container").appendChild(flipCard);
 
-const flipCardInner = document.createElement("div");
-flipCardInner.classList.add("flip-card-inner");
+    const flipCardFront = document.createElement("div");
+    flipCardFront.classList.add("flip-card-front");
 
-flipCard.appendChild(flipCardInner);
-document.querySelector(".container").appendChild(flipCard);
+    const h1 = document.createElement("h1");
+    h1.innerText = "Card Game";
+    flipCardFront.appendChild(h1);
 
-const flipCardFront = document.createElement("div");
-flipCardFront.classList.add("flip-card-front");
+    const flipCardBack = document.createElement("div");
+    flipCardBack.classList.add("flip-card-back", `bg-${color}`);
 
-const h1 = document.createElement("h1");
-h1.innerText = "Card Game";
-flipCardFront.appendChild(h1);
-
-const flipCardBack = document.createElement("div");
-flipCardBack.classList.add("flip-card-back", "bg-dodgerblue");
-
-flipCardInner.appendChild(flipCardFront);
-flipCardInner.appendChild(flipCardBack);
-
-
-
-const arr = [];
-
-for (let i = 0; i <=15; i++){
-    arr.push(flipCard)
-}
-
-arr.forEach(function (flipCard) {
-  console.log(flipCard)
-})
-
-console.log(arr)
+    flipCardInner.appendChild(flipCardFront);
+    flipCardInner.appendChild(flipCardBack);
+  }
+});
