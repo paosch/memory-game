83fd84e33b70a06da202c234d1018be398fe32ae
Paola Schiaffino <paola_schiaffino@yahoo.co.uk>
2019-08-28 21:38:00 +0100

    Randomize card colors

--- index.js
+++ index.js
@@ -1,28 +1,33 @@
 let colors = ["dodgerblue", "red", "green", "pink", "orange", "yellow", "black", "navy"]
 colors.map(color => colors.push(color))
 
-colors.forEach(function(color){
+let randomized_colors = []
+for (let i=0; i=colors.length; i++) {
+  randomized_colors.push(colors.splice(Math.floor(Math.random() * colors.length), 1)[0]);
+}
+
+randomized_colors.forEach(function(color){
   
-    const flipCard = document.createElement("div");
-    flipCard.classList.add("flip-card");
+  const flipCard = document.createElement("div");
+  flipCard.classList.add("flip-card");
 
-    const flipCardInner = document.createElement("div");
-    flipCardInner.classList.add("flip-card-inner");
+  const flipCardInner = document.createElement("div");
+  flipCardInner.classList.add("flip-card-inner");
 
-    flipCard.appendChild(flipCardInner);
-    document.querySelector(".container").appendChild(flipCard);
+  flipCard.appendChild(flipCardInner);
+  document.querySelector(".container").appendChild(flipCard);
 
-    const flipCardFront = document.createElement("div");
-    flipCardFront.classList.add("flip-card-front");
+  const flipCardFront = document.createElement("div");
+  flipCardFront.classList.add("flip-card-front");
 
-    const h1 = document.createElement("h1");
-    h1.innerText = "Card Game";
-    flipCardFront.appendChild(h1);
+  const h1 = document.createElement("h1");
+  h1.innerText = "Card Game";
+  flipCardFront.appendChild(h1);
 
-    const flipCardBack = document.createElement("div");
-    flipCardBack.classList.add("flip-card-back", `bg-${color}`);
+  const flipCardBack = document.createElement("div");
+  flipCardBack.classList.add("flip-card-back", `bg-${color}`);
 
-    flipCardInner.appendChild(flipCardFront);
-    flipCardInner.appendChild(flipCardBack);
-    
+  flipCardInner.appendChild(flipCardFront);
+  flipCardInner.appendChild(flipCardBack);
+
 });
