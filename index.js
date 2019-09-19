016c0625f60b4df22da1a6ccbaaa6fc9580a6565
Paola Schiaffino <paola_schiaffino@yahoo.co.uk>
2019-09-19 17:29:51 +0100

    Refactored game

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+let prevClickedCard, flipped_and_matched;
 const cards_total = 16;
+const total_pairs = cards_total / 2;
 const randomized_colors = [];
 const colors = ["dodgerblue", "red", "green", "pink", "orange", "yellow", "black", "navy"];
 
-for (let i = 0; i < cards_total / 2; i++) {
+for (let i = 0; i < total_pairs; i++) {
   randomized_colors.push(colors[i]);
   randomized_colors.push(colors[i]);
 }
@@ -11,14 +13,7 @@ function shuffle_array(array) {
   array.sort(() => Math.random() - 0.5);
 }
 
-shuffle_array(randomized_colors);
-
-let prevClickedCard;
-let flipped_and_matched = 0;
-
-
 function flipCardToBack(event) {
-
   const currentClickedCard = event.currentTarget;
   currentClickedCard.classList.add("flipped");
   if (prevClickedCard) {
@@ -29,7 +24,7 @@ function flipCardToBack(event) {
         prevClickedCard.classList.remove("flipped");
       }, 800);
     } else {
-      flipped_and_matched += 1;
+      flipped_and_matched ++;
     }
 
     setTimeout(function() {
@@ -40,38 +35,48 @@ function flipCardToBack(event) {
     prevClickedCard = currentClickedCard;
   }
 
-  if (flipped_and_matched === 8) {
+  if (flipped_and_matched === total_pairs) {
     setTimeout(function() {
       let all_cards = document.querySelectorAll(".flipped");
       all_cards.forEach(thecard => thecard.classList.remove("flipped"));
-      location.reload();
+      setTimeout(function() {
+        document.querySelector(".container").innerHTML = "";
+        startGame();
+      }, 800);
     }, 800);
   }
 }
 
-randomized_colors.forEach(function(color) {
-  const flipCard = document.createElement("div");
-  flipCard.classList.add("flip-card");
-  flipCard.setAttribute("data-color", color)
-  flipCard.addEventListener("click", flipCardToBack);
+function startGame() {
+  flipped_and_matched = 0;
+  shuffle_array(randomized_colors);
 
-  const flipCardInner = document.createElement("div");
-  flipCardInner.classList.add("flip-card-inner");
+  randomized_colors.forEach(function(color) {
+    const flipCard = document.createElement("div");
+    flipCard.classList.add("flip-card");
+    flipCard.setAttribute("data-color", color)
+    flipCard.addEventListener("click", flipCardToBack);
 
-  flipCard.appendChild(flipCardInner);
-  document.querySelector(".container").appendChild(flipCard);
+    const flipCardInner = document.createElement("div");
+    flipCardInner.classList.add("flip-card-inner");
 
-  const flipCardFront = document.createElement("div");
-  flipCardFront.classList.add("flip-card-front");
+    flipCard.appendChild(flipCardInner);
+    document.querySelector(".container").appendChild(flipCard);
 
-  const h1 = document.createElement("h1");
-  h1.innerText = "Card Game";
-  h1.classList.add("flip-card-front-h1")
-  flipCardFront.appendChild(h1);
+    const flipCardFront = document.createElement("div");
+    flipCardFront.classList.add("flip-card-front");
 
-  const flipCardBack = document.createElement("div");
-  flipCardBack.classList.add("flip-card-back", `bg-${color}`);
+    const h1 = document.createElement("h1");
+    h1.innerText = "Card Game";
+    h1.classList.add("flip-card-front-h1")
+    flipCardFront.appendChild(h1);
 
-  flipCardInner.appendChild(flipCardFront);
-  flipCardInner.appendChild(flipCardBack);
-});
+    const flipCardBack = document.createElement("div");
+    flipCardBack.classList.add("flip-card-back", `bg-${color}`);
+
+    flipCardInner.appendChild(flipCardFront);
+    flipCardInner.appendChild(flipCardBack);
+  });
+};
+
+startGame();
